623bc2bb8aca3c7eda23c83eade03950c7da88cf
Muhammad Imran <imran5244513@gmail.com>
2026-01-16 22:58:12 +0500

    feat: update socket server implementation with connection handling and CORS configuration

--- 1_snapcartFolder/socketServer/index.js
+++ 1_snapcartFolder/socketServer/index.js
@@ -1,11 +1,30 @@
 import express from "express";
 import http from "http";
 import dotenv from "dotenv";
+import { Server } from "socket.io";
+
 dotenv.config();
-const port = process.env.PORT || 5000;
+
 const app = express();
 const server = http.createServer(app);
 
+const port = process.env.PORT || 5000;
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.NEXT_BASE_URL,
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
 server.listen(port, () => {
-  console.log("server started at", port);
+  console.log("🚀 Server started at", port);
 });
